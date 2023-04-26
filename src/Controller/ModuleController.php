22403e722f779fb424a25976ed651c009697f497
pierrearthurdemengel <pademengel@gmail.com>
2023-04-26 14:04:31 +0200

    listModule ok

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Module;
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ModuleController extends AbstractController
 {
     #[Route('/module', name: 'app_module')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+                // récupère touts les stagiaire de la bdd en une collection d'objet entreprise
+                $modules = $doctrine->getRepository(Module::class)->findAll();
         return $this->render('module/index.html.twig', [
-            'controller_name' => 'ModuleController',
+            'modules' => $modules
         ]);
     }
 }
--- src/Entity/Module.php
+++ src/Entity/Module.php
@@ -47,4 +47,9 @@ class Module
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->nomModule." : ".$this->detail;
+    }
 }
