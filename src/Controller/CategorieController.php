a8e9584838559456df1f55b89a5691a9aae63cf5
pierrearthurdemengel <pademengel@gmail.com>
2023-04-26 14:18:30 +0200

    listCategorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // récupère touts les Categorie de la bdd en une collection d'objet entreprise
+        $categories = $doctrine->getRepository(Categorie::class)->findAll();
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'categories' => $categories
         ]);
     }
 }
